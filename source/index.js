e1fd6d1c9cf035d3f6ab3e4129d025e131f67c6e
sakura17 <79160277+sakurita17@users.noreply.github.com>
2022-06-16 18:56:28 -0500

    jaime6.16.22

---
+++ source/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 2020;
+const callback = () => console.log("Server started in port " + port);
+app.listen(2020, callback);
+
+const public = path.resolve(__dirname, '../public');
+app.use(express.static(public));
+
+const home = (req,res) => res.sendFile(path.resolve(__dirname, './views','home.html'));
+const login = (req,res) => res.sendFile(path.resolve(__dirname, './views','login.html'));
+const register = (req,res) => res.sendFile(path.resolve(__dirname, './views','register.html'));
+const ayuda = (req,res) => res.sendFile(path.resolve(__dirname, './views','ayuda.html'));
+const ofertas = (req,res) => res.sendFile(path.resolve(__dirname, './views','ofertas.html'));
+const vender = (req,res) => res.sendFile(path.resolve(__dirname, './views','vender.html'));
+const tiendasOficiales = (req,res) => res.sendFile(path.resolve(__dirname, './views','tiendasOficiales.html'));
+
+
+app.get('/', home)
+app.get('/login', login)
+app.get('/register', register)
+app.get('/ayuda', ayuda)
+app.get('/ayuda', ayuda)
+app.get('/ofertas', ofertas)
+app.get('/vender', vender)
+app.get('/tiendasOficiales', tiendasOficiales)
